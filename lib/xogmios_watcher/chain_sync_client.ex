4e50f5d6976737800f011b5160c518ef21f0cb05
Carlos Souza <carloshrsouza@gmail.com>
2024-01-31 10:09:21 -0500

    Kill process upon disconnect

--- lib/xogmios_watcher/chain_sync_client.ex
+++ lib/xogmios_watcher/chain_sync_client.ex
@@ -4,6 +4,7 @@ defmodule XogmiosWatcher.ChainSyncClient do
   require Logger
 
   def start_link(opts) do
+    opts = Keyword.merge(opts, restart: :permanent)
     Xogmios.start_chain_sync_link(__MODULE__, opts)
   end
 
@@ -22,6 +23,11 @@ defmodule XogmiosWatcher.ChainSyncClient do
 
   @impl true
   def handle_disconnect(_reason, state) do
+    # Attempt to terminate the process so that it's automatically restarted by the supervision
+    # tree, thus yielding a new connection to be established
+    pid = GenServer.whereis(self())
+    Process.exit(pid, :kill)
+
     Logger.info("ChainSyncClient disconnecting #{inspect(state)}")
     :ok
   end
